8cb01f3c0482dd18e6cb79127dcfd42c3665da08
HolbertonGab <7290@holbertonstudents.com>
2023-11-22 10:05:23 +0000

    update on header main.h

--- main.h
+++ main.h
@@ -4,13 +4,13 @@
 #include <stdio.h>
 /**
  * struct specifiers - new struct type defining conversion specifiers
- * @valid: a valid character
+ * @letter: the letter to return
  * @functions: the functions to associate
  *
  */
 typedef struct specifiers
 {
-	char *valid;
+	char *letter;
 	int (*functions)(va_list);
 } speci;
 
@@ -21,5 +21,5 @@ int print_string(va_list args);
 int print_percent(va_list args);
 int print_decimal(va_list args);
 int print_integer(va_list args);
-
+int (*get_pf_functions(char var_f))(va_list args);
 #endif
